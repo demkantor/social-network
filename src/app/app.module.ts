fa5fc28c0f2a521f4cd104646d7584807c456ceb
demkantor <demkantor@gmail.com>
2020-04-27 22:52:22 -0500

    client side skeleton up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BioComponent } from './components/bio/bio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
